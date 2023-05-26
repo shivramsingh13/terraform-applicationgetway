727d5054c8cd007335661c29f70b62f1a6ae32f0
shivramsingh13 <91517543+shivramsingh13@users.noreply.github.com>
2023-05-26 11:10:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -142,8 +142,8 @@ resource "azurerm_windows_virtual_machine" "vm" {
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer = "WindowsServer"
-    sku = 2019-Datacenter"
-    version = "latest"
+    sku = "2019-Datacenter"
+    version   = "latest"
     }
   }
   
